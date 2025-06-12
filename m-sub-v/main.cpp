a69015941a5b941b5c59414651c4b680721cc93e
jiayao.geng <jiayao.geng@sophgo.com>
2025-06-12 11:50:38 +0800

    m-sub-v

---
+++ m-sub-v/main.cpp
@@ -0,0 +1,16 @@
+#include "systemc.h"
+ 
+SC_MODULE(hello)
+{
+    SC_CTOR(hello)
+    {
+        cout<<"Hello, SystemC!"<<endl;
+    }
+};
+  
+//main.cpp
+int sc_main(int i, char* a[])
+{
+    hello h("hello");
+    return 0;
+}
